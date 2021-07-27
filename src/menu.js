fc010ff6f62307d8f86ce92af97a1c6d8b7c6ae3
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-27 18:17:48 +0500

    Add full menu page

--- src/menu.js
+++ src/menu.js
@@ -13,7 +13,7 @@ function createMenu(){
   menu.classList.add("col-6");
   menu.classList.add("mx-auto");
 
-  menu.appendChild(createMenuItem1("Plov", "Plov is Uzbek traditional food that is cooked with tons of rice sometimes, beacouse it is extremely delicious, every foreign guest says that 'Words really fail to describe its taste.' \n Required products: Rice, Carrot, Onion, Meat, Oil, Peas, Raisins, Solt, etc...", "35 %", Plov);)
+  menu.appendChild(createMenuItem1("Plov", "Plov is Uzbek traditional food that is cooked with tons of rice sometimes, beacouse it is extremely delicious, every foreign guest says that 'Words really fail to describe its taste.' \n Required products: Rice, Carrot, Onion, Meat, Oil, Peas, Raisins, Solt, etc...", "35$", Plov));
 
   menu.appendChild(createMenuItem2("Somsa", "Somsa is ver nutritious type of food that is cooked in Tandir. Also this foot is prepared in every regioun of Uzbekistan \n Required products: Flour, fat, Meat, Oinion, Egg, Solt, etc...", "20$", Somsa));
 
@@ -21,11 +21,12 @@ function createMenu(){
 
   menu.appendChild(createMenuItem2("Shashlik", "Shashlik, especially prepared at southern regions of Uzbekistan frequently, it is almost fully meaty food, and cooke with skewereds \n Required products: Meat, Marinade, Onions, etc...", "20$", Shashlik));
 
-  return menu;
+  parentdiv.appendChild(menu);
+  return parentdiv;
 }
 
 
-function createMenuItem1(name, desciption, cost, Img) {
+function createMenuItem1(name, descriptions, cost, Img) {
   const itemDiv = document.createElement("div")
   itemDiv.classList.add('menu-item')
   itemDiv.classList.add("black-back");
@@ -33,9 +34,11 @@ function createMenuItem1(name, desciption, cost, Img) {
   itemDiv.classList.add("flex-row");
   itemDiv.classList.add("col-10");
   itemDiv.classList.add("float-start");
+  itemDiv.classList.add("m-3");
 
-  const textDiv = createElement("div");
-  const nameOfFood = document.createElement("h6")
+  const textDiv = document.createElement("div");
+  textDiv.classList.add("p-2");
+  const nameOfFood = document.createElement("h4")
   nameOfFood.classList.add("font");
   nameOfFood.classList.add("text-center");
   nameOfFood.innerText = name
@@ -43,17 +46,19 @@ function createMenuItem1(name, desciption, cost, Img) {
 
   const desc = document.createElement("p")
   desc.classList.add("main-font");
-  desc.innerText = description;
+  desc.innerText = descriptions;
   textDiv.appendChild(desc);
 
   const price = document.createElement("h4")
   price.classList.add("main-font");
-  price.innerText = cost
+  price.innerText = cost;
+  textDiv.appendChild(price);
 
 
   const imgDiv = document.createElement("div");
   const myMenu = new Image();
   myMenu.src = Img;
+  myMenu.classList.add("p-3");
   myMenu.classList.add("menu-size");
   imgDiv.appendChild(myMenu);
 
@@ -63,7 +68,7 @@ function createMenuItem1(name, desciption, cost, Img) {
   return itemDiv;
 }
 
-function createMenuItem2(name, desciption, cost, Img) {
+function createMenuItem2(name, descriptions, cost, Img) {
   const itemDiv1 = document.createElement("div")
   itemDiv1.classList.add('menu-item')
   itemDiv1.classList.add("black-back");
@@ -71,9 +76,11 @@ function createMenuItem2(name, desciption, cost, Img) {
   itemDiv1.classList.add("flex-row");
   itemDiv1.classList.add("col-10");
   itemDiv1.classList.add("float-end");
+  itemDiv1.classList.add("m-3");
 
-  const textDiv1 = createElement("div");
-  const nameOfFood1 = document.createElement("h6")
+  const textDiv1 = document.createElement("div");
+  textDiv1.classList.add("p-2");
+  const nameOfFood1 = document.createElement("h4")
   nameOfFood1.classList.add("font");
   nameOfFood1.classList.add("text-center");
   nameOfFood1.innerText = name
@@ -81,17 +88,19 @@ function createMenuItem2(name, desciption, cost, Img) {
 
   const desc1 = document.createElement("p")
   desc1.classList.add("main-font");
-  desc1.innerText = description;
+  desc1.innerText = descriptions;
   textDiv1.appendChild(desc1);
 
   const price1 = document.createElement("h4")
   price1.classList.add("main-font");
   price1.innerText = cost
+  textDiv1.appendChild(price1);
 
 
   const imgDiv1 = document.createElement("div");
   const myMenu1 = new Image();
   myMenu1.src = Img;
+  myMenu1.classList.add("p-3");
   myMenu1.classList.add("menu-size");
   imgDiv1.appendChild(myMenu1);
 
