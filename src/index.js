229d7c2f18976f7dbc508b6682f9968d29667f41
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-27 21:19:20 +0500

    Correct linter errors

--- src/index.js
+++ src/index.js
@@ -1,57 +1,49 @@
-import _, { create } from 'lodash';
+import _ from 'lodash';
 import './style.css';
 import Logo from './header-logo.png';
 import loadHome from './home';
 import loadMenu from './menu';
-import loadContact from './contact'
+import loadContact from './contact';
 
-const container = document.querySelector(".container1");
-function createHeader() {
-  const header = document.createElement("header");
-  header.classList.add("text-center");
-  header.classList.add("black-back");
-  header.innerHTML = '<h1 class="text-white py-4 font-style">NestOne Restaurant</h1>'
-  const text = document.createElement("h4");
-  text.innerText = "Reastaurant is one of the thirty three services of Nest Zone"
-  text.classList.add("text-white");
-  text.classList.add("font");
-  header.appendChild(text);
-  header.appendChild(createNavbar());
-  return header;
+const container = document.querySelector('.container1');
+
+
+function addClass(button) {
+  button.classList.add('button-nav');
+  button.classList.add('mx-3');
+  button.classList.add('main-font');
 }
 
 function createNavbar() {
-  const nav = document.createElement("nav");
-  nav.classList.add("py-3");
+  const nav = document.createElement('nav');
+  nav.classList.add('py-3');
 
-  const homeButton = document.createElement("button");
+  const homeButton = document.createElement('button');
   addClass(homeButton);
   homeButton.innerHTML = '<h5>Home</h5>';
-  homeButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  homeButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(homeButton);
     loadHome();
   });
 
-
-  const menuButton = document.createElement("button");
+  const menuButton = document.createElement('button');
   addClass(menuButton);
-  menuButton.innerHTML = "<h5>Menu</h5>";
-  menuButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  menuButton.innerHTML = '<h5>Menu</h5>';
+  menuButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(menuButton);
     loadMenu();
   });
 
-  const contactButton = document.createElement("button");
+  const contactButton = document.createElement('button');
   addClass(contactButton);
   contactButton.innerHTML = '<h5>Contact</h5>';
-  contactButton.addEventListener("click", (e) => {
-    if (e.target.classList.contains("active")) return;
+  contactButton.addEventListener('click', (e) => {
+    if (e.target.classList.contains('active')) return;
     setActiveButton(contactButton);
     loadContact();
   });
-  
 
   nav.appendChild(homeButton);
   nav.appendChild(menuButton);
@@ -60,88 +52,94 @@ function createNavbar() {
   return nav;
 }
 
-function addClass(button) {
-  button.classList.add("button-nav");
-  button.classList.add("mx-3");
-  button.classList.add("main-font");
+function createHeader() {
+  const header = document.createElement('header');
+  header.classList.add('text-center');
+  header.classList.add('black-back');
+  header.innerHTML = '<h1 class="text-white py-4 font-style">NestOne Restaurant</h1>';
+  const text = document.createElement('h4');
+  text.innerText = 'Reastaurant is one of the thirty three services of Nest Zone';
+  text.classList.add('text-white');
+  text.classList.add('font');
+  header.appendChild(text);
+  header.appendChild(createNavbar());
+  return header;
 }
 
 function setActiveButton(button) {
-  const buttons = document.querySelectorAll(".button-nav");
+  const buttons = document.querySelectorAll('.button-nav');
 
   buttons.forEach((button) => {
     if (button !== this) {
-      button.classList.remove("active");
+      button.classList.remove('active');
     }
   });
 
-  button.classList.add("active");
+  button.classList.add('active');
 }
 
 function createMain() {
-  const main = document.createElement("main");
-  main.classList.add("main");
-  main.setAttribute("id", "main");
+  const main = document.createElement('main');
+  main.classList.add('main');
+  main.setAttribute('id', 'main');
   return main;
 }
 
+function createFooter() {
+  const footer = document.createElement('footer');
+  footer.classList.add('black-back');
+  footer.classList.add('mt-5');
 
-function createFooter(){
-  const footer = document.createElement("footer");
-  footer.classList.add("black-back");
-  footer.classList.add("mt-5");
+  const inner_div = document.createElement('div');
+  inner_div.classList.add('px-4');
+  inner_div.classList.add('py-3');
+  inner_div.classList.add('d-flex');
+  inner_div.classList.add('flex-row');
+  inner_div.classList.add('justify-content-between');
 
-  const inner_div = document.createElement("div");
-  inner_div.classList.add("px-4");
-  inner_div.classList.add("py-3");
-  inner_div.classList.add("d-flex");
-  inner_div.classList.add("flex-row");
-  inner_div.classList.add("justify-content-between");
-
-  const first_div = document.createElement("div");
+  const first_div = document.createElement('div');
   const myLogo = new Image();
   myLogo.src = Logo;
   first_div.appendChild(myLogo);
 
-  const copy = document.createElement("small");
-  copy.classList.add("d-block");
-  copy.classList.add("text-white");
-  copy.classList.add("main-font");
-  copy.innerText = "© 2021 | Zulfizar Abdumurodova";
+  const copy = document.createElement('small');
+  copy.classList.add('d-block');
+  copy.classList.add('text-white');
+  copy.classList.add('main-font');
+  copy.innerText = '© 2021 | Zulfizar Abdumurodova';
   first_div.appendChild(copy);
 
-  const second_div = document.createElement("div");
-  second_div.classList.add("d-flex");
-  second_div.classList.add("flex-row");
-  second_div.classList.add("pt-3");
+  const second_div = document.createElement('div');
+  second_div.classList.add('d-flex');
+  second_div.classList.add('flex-row');
+  second_div.classList.add('pt-3');
 
-  const item1 = document.createElement("div");
-  const link1 = document.createElement("a");
-  link1.setAttribute("href", "https://www.linkedin.com/in/zulfizarabdumurodova/");
-  link1.classList.add("text-white");
+  const item1 = document.createElement('div');
+  const link1 = document.createElement('a');
+  link1.setAttribute('href', 'https://www.linkedin.com/in/zulfizarabdumurodova/');
+  link1.classList.add('text-white');
   link1.innerHTML = '<i class="fab fa-linkedin fa-2x px-3"></i>';
-  const item2 = document.createElement("div");
-  const link2 = document.createElement("a");
-  link2.setAttribute("href", "https://github.com/AbdumurodovaZulfizar");
-  link2.classList.add("text-white");
+  const item2 = document.createElement('div');
+  const link2 = document.createElement('a');
+  link2.setAttribute('href', 'https://github.com/AbdumurodovaZulfizar');
+  link2.classList.add('text-white');
   link2.innerHTML = '<i class="fab fa-github fa-2x"></i>';
   item1.appendChild(link1);
   item2.appendChild(link2);
   second_div.appendChild(item1);
   second_div.appendChild(item2);
   inner_div.appendChild(first_div);
-  inner_div.appendChild(second_div)
+  inner_div.appendChild(second_div);
   footer.appendChild(inner_div);
   return footer;
 }
 
-
-function createWebsite(){
+function createWebsite() {
   container.appendChild(createHeader());
   container.appendChild(createMain());
   container.appendChild(createFooter());
 
-  setActiveButton(document.querySelector(".button-nav"));
+  setActiveButton(document.querySelector('.button-nav'));
   loadHome();
 }
 
