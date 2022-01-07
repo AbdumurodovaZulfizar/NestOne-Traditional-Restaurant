4b2fa5daa62442cd7643178fb9c8da8ce7886bc5
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2022-01-07 09:40:30 +0500

    Make the map responsie

--- src/contact.js
+++ src/contact.js
@@ -9,6 +9,7 @@ const createContact = () => {
   contact.classList.add('col-12');
   contact.classList.add('col-lg-6');
   contact.classList.add('mx-auto');
+  contact.classList.add('px-0')
 
   const contactDiv = document.createElement('div');
   contactDiv.classList.add('black-back');
@@ -27,7 +28,7 @@ const createContact = () => {
   mapword.innerHTML = '<h3>NestOne Location:</h3>';
   mapDiv.classList.add('font');
   mapDiv.appendChild(mapword);
-  maptext.innerHTML = '<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Uzbekistan Tashkent NestOne&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.fridaynightfunkin.net/friday-night-funkin-mods-fnf-play-online/">FNF Mods</a></div><style>.mapouter{position:relative;text-align:right;width:600px;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:600px;height:400px;}.gmap_iframe {width:600px!important;height:400px!important;}</style></div>';
+  maptext.innerHTML = '<div class="mapouter"><div class="gmap_canvas"><iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Uzbekistan Tashkent NestOne&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.fridaynightfunkin.net/friday-night-funkin-mods-fnf-play-online/">FNF Mods</a></div><style>.mapouter{position:relative;text-align:right;width:100%;height:400px;}.gmap_canvas {overflow:hidden;background:none!important;width:100%;height:400px;}.gmap_iframe {width:100%!important;height:400px!important;}</style></div>';
   mapDiv.appendChild(maptext);
 
   const phoneNumber = document.createElement('h3');
